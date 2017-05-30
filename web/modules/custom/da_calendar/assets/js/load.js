58796d169db6f7bf544173534ce9231da7765311
Hangea Cristian <cristian.hangea@pitechnologies.ro>
2017-05-30 16:29:18 +0300

    Create Calendar Event Feed. See #37.

--- web/modules/custom/da_calendar/assets/js/load.js
+++ web/modules/custom/da_calendar/assets/js/load.js
@@ -2,10 +2,13 @@
   Drupal.behaviors.da_calendar_load_calendar_entries = {
     attach: function (context, settings) {
       $('#calendar').fullCalendar({
-        eventSources: [
-
-        ]
-      });
+        locale: document.documentElement.lang,
+        events : '/events/calendar',
+        displayEventTime: false,
+        error: function() {
+          alert('there was an error while fetching events!');
+        }
+      })
     }
   }
 })(jQuery, Drupal);
